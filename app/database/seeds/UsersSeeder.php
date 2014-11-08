2fa3cbc8b965e2b6fa19b52e289304993308c799
Lovro Predovan <lovro.predovan@gmail.com>
2014-11-08 15:39:42 +0100

    added Faker for seeding data to Users

--- app/database/seeds/UsersSeeder.php
+++ app/database/seeds/UsersSeeder.php
@@ -6,7 +6,7 @@ class UsersSeeder extends DatabaseSeeder
 
     public function run()
     {
-        //we have to make some improvements
+        //basic users
         $users = [
             [
                 "name" => "Admin",
@@ -34,7 +34,7 @@ class UsersSeeder extends DatabaseSeeder
                 "name" => "User",
                 "surname" => "User",
                 "username"    => "user",
-                "password"    => Hash::make("user"),
+                "password"    => Hash::make("user123"),
                 "phone"    => "000000",
                 "avatar"    => "https://success.salesforce.com/resource/1414972800000/sharedlayout/img/new-user-image-default.png",
                 "activated"    => "0",
@@ -42,10 +42,28 @@ class UsersSeeder extends DatabaseSeeder
                 "activation_token"    => Hash::make("user")
             ]
         ];
-
         foreach ($users as $user)
         {
             User::create($user);
+        }
+
+        //all other users
+        $faker = Faker\Factory::create();
+
+        for ($i = 0; $i < 100; $i++)
+        {
+                User::create(array(
+                "name" => $faker->name,
+                "surname" => $faker->lastname,
+                "username"    => $faker->userName,
+                "password"    => Hash::make("user123"),
+                "phone"    => $faker->phoneNumber,
+                "email"    => $faker->email,
+                "avatar"    => $faker->imageUrl($width = 200, $height = 200),
+                "activated"    => "0",
+                "role_id"    => "3",
+                "activation_token"    => Hash::make("user")
+            ));
         }
     }
 
